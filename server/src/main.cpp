fc2bd91253ad4004bd868b453ee1434b969f3ff2
Ryan <ryanlimjk@gmail.com>
2026-02-12 15:41:42 +0800

    server dockerised

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,7 +1,26 @@
 #include "core/Server.hpp"
 #include <iostream>
+#include <atomic>
+#include <csignal>
+#include <mutex>
+#include <condition_variable>
+
+std::atomic<bool> running(true);
+std::mutex mtx;
+std::condition_variable cv;
+
+void handleSignal(int) {
+    running = false;
+    cv.notify_all(); // wake main thread
+}
 
 int main() {
+    std::cout.setf(std::ios::unitbuf); // automatic flush after every output
+
+    // Handle Ctrl+C and Docker stop
+    std::signal(SIGINT, handleSignal);
+    std::signal(SIGTERM, handleSignal);
+
     TcpServer server(4000);
 
     if (!server.start()) {
@@ -9,9 +28,14 @@ int main() {
         return 1;
     }
 
-    std::cout << "Server running.\n";
-    std::cin.get();
+    std::cout << "Server running...\n";
 
+    // Industry-standard: main thread blocks, zero CPU usage
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [](){ return !running.load(); });
+
+    std::cout << "Shutting down server...\n";
     server.stop();
+
     return 0;
 }
