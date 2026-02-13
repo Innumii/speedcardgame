89547dd5068bce8647be0a1b0a89aa20d909d01b
innumii <linus.phong.2022@scis.smu.edu.sg>
2026-02-13 19:04:04 +0800

    new cards

--- cards/services/card.service.go
+++ cards/services/card.service.go
@@ -1,75 +1,69 @@
 package services
 
 import (
-        "net/http"
-		"encoding/json"
-		"fmt"
-        
-        "github.com/Ryanljk/speedcardgame/cards/config"
-        "github.com/Ryanljk/speedcardgame/cards/models"
-	)
+	"encoding/json"
+	"fmt"
+	"net/http"
 
-const (
-    PageSize = 8
+	"github.com/Ryanljk/speedcardgame/cards/config"
+	"github.com/Ryanljk/speedcardgame/cards/models"
 )
 
 var inputCard struct {
-        Uid       int    `json:"uid"`
-        Cid       int    `json:"cid"`
-        Name      string `json:"name"`
-        Type      string `json:"type"`
-        Cost      int    `json:"cost"`
-        Value     int    `json:"value"`
-        Power     int    `json:"power"`
-        Toughness int    `json:"toughness"`
-        Effect    string `json:"effect"`
-    }
+	Uid       int    `json:"uid"`
+	Cid       int    `json:"cid"`
+	Name      string `json:"name"`
+	Type      string `json:"type"`
+	Cost      int    `json:"cost"`
+	Value     int    `json:"value"`
+	Power     int    `json:"power"`
+	Toughness int    `json:"toughness"`
+	Effect    string `json:"effect"`
+}
 
 // create a new card, ONLY FOR ADMIN USE
 func CreateCard(w http.ResponseWriter, r *http.Request) {
 
-    // Decode the incoming request body
-    if err := json.NewDecoder(r.Body).Decode(&inputCard); err != nil {
-        http.Error(w, "Invalid inputCard", http.StatusBadRequest)
-        return
-    }
+	// Decode the incoming request body
+	if err := json.NewDecoder(r.Body).Decode(&inputCard); err != nil {
+		http.Error(w, "Invalid inputCard", http.StatusBadRequest)
+		return
+	}
 
-    // TODO: Add admin authentication check here
+	// TODO: Add admin authentication check here
 
-    // Create a new Card instance
-    card := models.Card{
-        Cid:      inputCard.Cid,
-        Name:     inputCard.Name,
-        Type:     inputCard.Type,
-        Cost:     inputCard.Cost,
-        Value:    inputCard.Value,
-        Power:    inputCard.Power,
-        Toughness: inputCard.Toughness,
-        Effect:   inputCard.Effect,
-    }
+	// Create a new Card instance
+	card := models.Card{
+		Cid:       inputCard.Cid,
+		Name:      inputCard.Name,
+		Type:      inputCard.Type,
+		Cost:      inputCard.Cost,
+		Value:     inputCard.Value,
+		Power:     inputCard.Power,
+		Toughness: inputCard.Toughness,
+		Effect:    inputCard.Effect,
+	}
 
-    // Save the card to the database
-    if err := config.DB.Create(&card).Error; err != nil {
-        http.Error(w, fmt.Sprintf("Failed to create card: %v", err), http.StatusInternalServerError)
-        return
-    }
+	// Save the card to the database
+	if err := config.DB.Create(&card).Error; err != nil {
+		http.Error(w, fmt.Sprintf("Failed to create card: %v", err), http.StatusInternalServerError)
+		return
+	}
 
-    // Return the created card
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(card)
+	// Return the created card
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(card)
 }
 
 // List cards within the page range by Cid order
 func ListCards(w http.ResponseWriter, _ *http.Request) {
 	var cards []models.Card
-    startPage := 0      // Default to first page
-    endPage := PageSize // Default to first page size
 
-	// Query for cards ordered by Cid within the specified page range
-    if result := config.DB.Order("cid").Offset(startPage).Limit(endPage - startPage).Find(&cards); result.Error != nil {
-        http.Error(w, result.Error.Error(), http.StatusInternalServerError)
-        return
-    }
+	// Query for all cards ordered by Cid so the client can paginate locally
+	if result := config.DB.Order("cid").Find(&cards); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	// Respond with the list of decks
 	w.Header().Set("Content-Type", "application/json")
@@ -79,79 +73,77 @@ func ListCards(w http.ResponseWriter, _ *http.Request) {
 // Update Card details, ONLY FOR ADMIN USE
 func UpdateCard(w http.ResponseWriter, r *http.Request) {
 
-    // TODO: Add admin authentication check here
+	// TODO: Add admin authentication check here
 
-    // Decode the incoming request body
-    if err := json.NewDecoder(r.Body).Decode(&inputCard); err != nil {
-        http.Error(w, "Invalid inputCard", http.StatusBadRequest)
-        return
-    }
+	// Decode the incoming request body
+	if err := json.NewDecoder(r.Body).Decode(&inputCard); err != nil {
+		http.Error(w, "Invalid inputCard", http.StatusBadRequest)
+		return
+	}
 
-    // Extract the CardID from the URL parameter
-    cid := inputCard.Cid
+	// Extract the CardID from the URL parameter
+	cid := inputCard.Cid
 
-    // Find the existing card
-    var card models.Card
-    if err := config.DB.First(&card, cid).Error; err != nil {
-        http.Error(w, fmt.Sprintf("Card not found: %v", err), http.StatusNotFound)
-        return
-    }
+	// Find the existing card
+	var card models.Card
+	if err := config.DB.First(&card, cid).Error; err != nil {
+		http.Error(w, fmt.Sprintf("Card not found: %v", err), http.StatusNotFound)
+		return
+	}
 
-    // Apply updates, if the field is empty, keep the existing value
-    if inputCard.Name != "" {
-        card.Name = inputCard.Name
-    } else if inputCard.Type != "" {
-        card.Type = inputCard.Type
-    } else if inputCard.Cost > -1 {
-        card.Cost = inputCard.Cost
-    } else if inputCard.Value > -1 {
-        card.Value = inputCard.Value
-    } else if inputCard.Power > -1 {
-        card.Power = inputCard.Power
-    } else if inputCard.Toughness > -1 {
-        card.Toughness = inputCard.Toughness
-    } else if inputCard.Effect != "" {
-        card.Effect = inputCard.Effect
-    } else {
-        http.Error(w, "No valid fields to update", http.StatusBadRequest)
-        return
-    }
+	// Apply updates, if the field is empty, keep the existing value
+	if inputCard.Name != "" {
+		card.Name = inputCard.Name
+	} else if inputCard.Type != "" {
+		card.Type = inputCard.Type
+	} else if inputCard.Cost > -1 {
+		card.Cost = inputCard.Cost
+	} else if inputCard.Value > -1 {
+		card.Value = inputCard.Value
+	} else if inputCard.Power > -1 {
+		card.Power = inputCard.Power
+	} else if inputCard.Toughness > -1 {
+		card.Toughness = inputCard.Toughness
+	} else if inputCard.Effect != "" {
+		card.Effect = inputCard.Effect
+	} else {
+		http.Error(w, "No valid fields to update", http.StatusBadRequest)
+		return
+	}
 
-    // Save the updated card
-    if err := config.DB.Save(&card).Error; err != nil {
-        http.Error(w, fmt.Sprintf("Failed to update card: %v", err), http.StatusInternalServerError)
-        return
-    }
+	// Save the updated card
+	if err := config.DB.Save(&card).Error; err != nil {
+		http.Error(w, fmt.Sprintf("Failed to update card: %v", err), http.StatusInternalServerError)
+		return
+	}
 
-    // Return the updated card
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(card)
+	// Return the updated card
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(card)
 }
 
 func DeleteCard(w http.ResponseWriter, r *http.Request) {
-    if err := json.NewDecoder(r.Body).Decode(&inputCard); err != nil {
-        http.Error(w, "Invalid inputCard", http.StatusBadRequest)
-        return
-    }
+	if err := json.NewDecoder(r.Body).Decode(&inputCard); err != nil {
+		http.Error(w, "Invalid inputCard", http.StatusBadRequest)
+		return
+	}
 
-    // Extract the CardID from the URL parameter
-    cid := inputCard.Cid
+	// Extract the CardID from the URL parameter
+	cid := inputCard.Cid
 
+	// Find the card to delete
+	var card models.Card
+	if err := config.DB.First(&card, cid).Error; err != nil {
+		http.Error(w, fmt.Sprintf("Card not found: %v", err), http.StatusNotFound)
+		return
+	}
 
-    // Find the card to delete
-    var card models.Card
-    if err := config.DB.First(&card, cid).Error; err != nil {
-        http.Error(w, fmt.Sprintf("Card not found: %v", err), http.StatusNotFound)
-        return
-    }
-
-    // Delete the card
-    if err := config.DB.Delete(&card).Error; err != nil {
-        http.Error(w, fmt.Sprintf("Failed to delete card: %v", err), http.StatusInternalServerError)
-        return
-    }
+	// Delete the card
+	if err := config.DB.Delete(&card).Error; err != nil {
+		http.Error(w, fmt.Sprintf("Failed to delete card: %v", err), http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(card)
+	json.NewEncoder(w).Encode(card)
 }
-
