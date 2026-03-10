61f719b411d34ebb09481d4f437da8a263ae5d4d
Ryan <ryanlimjk@gmail.com>
2026-03-11 00:11:40 +0800

    debugged entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,14 +2,12 @@
 set -e
 
 # Create the certs directory
-mkdir -p /certs
-
-# Write TLS certs from environment variables
-echo "$TLS_CERT" > /certs/server.crt
-echo "$TLS_KEY"  > /certs/server.key
+mkdir -p /tmp/certs
+printf "%s" "$TLS_CERT" > /tmp/certs/server.crt
+printf "%s" "$TLS_KEY" > /tmp/certs/server.key
 
 # Secure the files
-chmod 600 /certs/server.*
+chmod 600 /tmp/certs/server.*
 
 # Execute the original server binary
 exec /app/server "$@"
