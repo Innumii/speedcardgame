97b30076e55522e920af7c61460dd5ac92f4355f
Ryan <ryanlimjk@gmail.com>
2026-03-20 17:03:46 +0800

    Cleanup

--- server/src/game/TriggerEffects.cpp
+++ server/src/game/TriggerEffects.cpp
@@ -124,15 +124,15 @@ const std::vector<CardEffectEntry>* TriggerEffects::getCardEffects(int cardId) {
 
 const std::unordered_map<int, std::vector<CardEffectEntry>> TriggerEffects::cardToEffectsMap = {
     {1, {CardEffectEntry{0, std::nullopt, std::make_pair(0,-3)} }}, //done
-        {4, { CardEffectEntry{0, std::nullopt, std::make_pair(4,4), //add conditional augment
-            [](const MatchSession& session, int cardId, int, int) {
-            // std::cout<< "[Effect Condition] Given Name: "<<session.getCard(cardId)->getName()<< "\n";
-            return session.getCard(cardId)->getName() == "Go";}},
-            CardEffectEntry{0, std::nullopt, std::make_pair(2,2), //add conditional augment
-            [](const MatchSession& session, int cardId, int, int) {
-            return session.getCard(cardId)->getName() != "Go";}} 
-            }
-        }, 
+    {4, { CardEffectEntry{0, std::nullopt, std::make_pair(4,4), //add conditional augment
+        [](const MatchSession& session, int cardId, int, int) {
+        // std::cout<< "[Effect Condition] Given Name: "<<session.getCard(cardId)->getName()<< "\n";
+        return session.getCard(cardId)->getName() == "Go";}},
+        CardEffectEntry{0, std::nullopt, std::make_pair(2,2), //add conditional augment
+        [](const MatchSession& session, int cardId, int, int) {
+        return session.getCard(cardId)->getName() != "Go";}} 
+        }
+    }, 
     {10, { CardEffectEntry{4, std::nullopt, std::make_pair(0,1)}}}, //done 
     {11, { CardEffectEntry{2, std::nullopt, std::make_pair(0,-1)}}}, //done
         {13, { CardEffectEntry{0, std::nullopt, std::make_pair(5,-1),
@@ -144,18 +144,14 @@ const std::unordered_map<int, std::vector<CardEffectEntry>> TriggerEffects::card
              }
         }, //conditonal +5/-1
     {14, { CardEffectEntry{0, std::nullopt, std::make_pair(2,-1)}}}, //done
-        {15, { CardEffectEntry{7, CombatEffects::kDoubleStrike, std::nullopt} }}, // Give Double Strike
+    {15, { CardEffectEntry{7, CombatEffects::kDoubleStrike, std::nullopt} }}, // Give Double Strike
     {17, { CardEffectEntry{0, std::nullopt, std::make_pair(-1,-1)}}}, //done
-        {18, { CardEffectEntry{7, CombatEffects::kTrample, std::nullopt} }}, // Give Trample
+    {18, { CardEffectEntry{7, CombatEffects::kTrample, std::nullopt} }}, // Give Trample
     {20, { CardEffectEntry{0, std::nullopt, std::make_pair(0,3)} }}, //done
     {23, { CardEffectEntry{3, -5, std::nullopt}, //done
         CardEffectEntry{5, 5, std::nullopt} }}, 
-
-
-        {25, { CardEffectEntry{10, 2, std::nullopt}}}, // Conditional splash HP augment
-    
-    
-        {26, {CardEffectEntry{2, std::nullopt, std::make_pair(2,2)}}}, //done
-        {29, { CardEffectEntry{6, 10, std::nullopt}}}, // Deal difference as damage
-        {32, { CardEffectEntry{7, CombatEffects::kDeathTouch, std::nullopt} }} // Cheese Touch: Target creature gains Deathtouch
+    {25, { CardEffectEntry{10, 2, std::nullopt}}}, // Conditional splash HP augment
+    {26, {CardEffectEntry{2, std::nullopt, std::make_pair(2,2)}}}, //done
+    {29, { CardEffectEntry{6, 10, std::nullopt}}}, // Deal difference as damage
+    {32, { CardEffectEntry{7, CombatEffects::kDeathTouch, std::nullopt} }} // Cheese Touch: Target creature gains Deathtouch
 };
